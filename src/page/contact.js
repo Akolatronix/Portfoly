3d1fc1ac3010a2677596263810b5ba4c3cc1f6c3
Akolatronix <146489836+Akolatronix@users.noreply.github.com>
2024-06-08 21:07:33 -0400

    Add files via upload

--- src/page/contact.js
+++ src/page/contact.js
@@ -13,15 +13,21 @@ export default function Contact() {
             <form className={`${styles.form} ${isMounted ? styles.visible : ''}`}>
                 <h1>Formulario de Contacto</h1>
                 <div className={styles.inputGroup}>
-                            <input className={styles.input} type="text" id="name" name="name" placeholder='nombre' required />
+                    <input className={styles.input} type="text" id="name" name="name" placeholder='nombre' required />
+                    <input className={styles.input} type="tel" id="email" name="email" placeholder='Apellido' required />
                 </div>
                 <div className={styles.inputGroup}>
-                            <input className={styles.input} type="email" id="email" name="email" placeholder='Apellido' required />
+                    <input className={styles.input} type="text" id="name" name="name" placeholder='Telefono' required />
                 </div>
                 <div className={styles.inputGroup}>
-                <textarea className={styles.input} id="message" name="message" placeholder='Tu mensaje' required></textarea>
+                    <input className={styles.input} type="email" id="name" name="name" placeholder='Correo' required />
                 </div>
-                <button type="submit">Enviar</button>
+                <div className={styles.inputGroup}>
+                    <textarea className={styles.input} id="message" name="message" placeholder='Tu mensaje' required></textarea>
+                </div>
+                <div className={styles.inputButton}>
+                    <button className={styles.button} type="submit">Enviar</button>
+                </div>
             </form>
         </div>
     )
